3d5d44d3dc468dc838d6ef14c5a684cabec9daa3
faker1897 <yangyunsong040529@163.com>
2025-04-13 17:42:57 -0700

    update config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/TeamPage/',
+  deploymentBranch: 'deploy',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'faker1897', // Usually your GitHub org/user name.
